2f0e5a8262b19e54eead9ab39e59e6893149a061
Rodrigo Calmon <rmfontes@viceri.com.br>
2023-10-25 15:31:07 -0300

    criação de projeto ms-demo(user) para relembrar pequenos conceitos sobre microservices

---
+++ demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
